3f34e5f43dc3c06b4b0bfc57baf9517b02859c87
tolbertinnovation-debug <tolbertinnovation@gmail.com>
2026-09-22 18:21:28 -0600

    Balance mobile spacing across site sections and heroes

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.documentElement.classList.add('js');
 if (!document.querySelector('link[data-irf-enhancements]')) {
   const enhancementStyles = document.createElement('link');
   enhancementStyles.rel = 'stylesheet';
-  enhancementStyles.href = new URL('enhancements.css?v=20260923-2', document.currentScript.src).href;
+  enhancementStyles.href = new URL('enhancements.css?v=20260923-3', document.currentScript.src).href;
   enhancementStyles.dataset.irfEnhancements = 'true';
   document.head.appendChild(enhancementStyles);
 }
